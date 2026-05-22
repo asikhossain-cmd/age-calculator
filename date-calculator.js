7688e74690fb82ec7c1c696eb4f4295aa767ce14
Asik <asikhossain592445@gmail.com>
2026-05-22 20:54:12 +0600

    date calculator design and basic functionality added

---
+++ date-calculator.js
@@ -0,0 +1,9 @@
+
+const getButton = document.getElementById('button');
+
+getButton.addEventListener('click', () => {
+    // Get DoB Date
+    const getDobData = document.getElementById('bdate');
+    
+    // Get Current Date
+})
