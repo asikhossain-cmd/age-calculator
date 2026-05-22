ef4f5508f64dda6a38279c679b9136f6902d2685
Asik <asikhossain592445@gmail.com>
2026-05-22 21:56:26 +0600

    Age Functionality Implemented Successfully

--- date-calculator.js
+++ date-calculator.js
@@ -10,17 +10,25 @@ getButton.addEventListener('click', () => {
     
     //Convert Dob Date and Current Date to Object
     const getDobDateToObj = new Date(getDobData.value);
-
     const getCurrentDateObj = new Date(getCurrentDate.value);
 
     // Extract Year and Months
     const getYearDoB = getDobDateToObj.getFullYear();
     const getMonthDoB = getDobDateToObj.getMonth();
-    const getDayDoB = getDobDateToObj.getDay();
+    const getDayDoB = getDobDateToObj.getDate();
 
-    const getYearCdate = getCurrentDate.getFullYear();
-    const getMonthCdate = getCurrentDate.getMonth();
-    const getDayCdate = getCurrentDate.getDay();
 
+    const getYearCdate = getCurrentDateObj.getFullYear();
+    const getMonthCdate = getCurrentDateObj.getMonth();
+    const getDayCdate = getCurrentDateObj.getDate();
+
+    // Calculate Year and Full Age
+    let getYear = getYearCdate - getYearDoB - 1;
+
+    // Check if Pass 12 Month then add 1 year to getYear
+    if(getMonthDoB >= getMonthCdate && getDayCdate >= getDayDoB) {
+        getYear += 1
+    } 
+    console.log(getYear)
     
 })
